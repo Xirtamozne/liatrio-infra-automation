223f26a131d7083d6e986cfb14103df9f30bf5f7
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-19 16:26:44 +0200

    Initialized repo...

--- initRepo.sh
+++ initRepo.sh
@@ -4,5 +4,5 @@ touch README.md
 git init
 git add -A .
 git commit -m "Initialized repo..."
-git remote add origin https://github.com/Xirtamozne/liatrio-infra-automation.git
-git push -u origin master
+#git remote add origin https://github.com/Xirtamozne/liatrio-infra-automation.git
+git push -u origin2 master
