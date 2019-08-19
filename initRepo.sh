a195b3f64b3688c7f47a2ceca8dff0f7ae6e9793
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-19 16:17:10 +0200

    Initialized repo...

---
+++ initRepo.sh
@@ -0,0 +1,8 @@
+git config --global user.name "Xirtamozne"
+git config --global user.email Johnny.E.Hughes@gmail.com
+touch README.md
+git init
+git add -A .
+git commit -m "Initialized repo..."
+git remote add origin https://github.com/Xirtamozne/liatrio-infra-automation.git
+git push -u origin master
