2b01c3312f0d1b44b0cd807139ed7cd3a1793a38
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-20 22:54:54 -0600

    Delete initRepo.sh

--- initRepo.sh
+++
@@ -1,8 +0,0 @@
-git config --global user.name "Xirtamozne"
-git config --global user.email Johnny.E.Hughes@gmail.com
-touch README.md
-git init
-git add -A .
-git commit -m "Initialized repo..."
-#git remote add origin https://github.com/Xirtamozne/liatrio-infra-automation.git
-git push -u origin2 master
