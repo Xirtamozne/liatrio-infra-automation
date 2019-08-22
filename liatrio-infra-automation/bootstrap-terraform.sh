24c96e67addcbfe9282fb1d22a867e93b2cc92aa
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-22 04:01:49 +0200

    Adding better instance IP handling.

--- liatrio-infra-automation/bootstrap-terraform.sh
+++ liatrio-infra-automation/bootstrap-terraform.sh
@@ -3,6 +3,7 @@ chmod +w ~/.ssh/LiatrioKeyPairFinal.pem 2> /dev/null
 echo "Host * " > ~/.ssh/config
 echo "    StrictHostKeyChecking no " >> ~/.ssh/config
 rm -f ~/.ssh/LiatrioKeyPairFinal.pem
+aws ec2 delete-key-pair --key-name LiatrioKeyPairFinal 
 aws ec2 create-key-pair --key-name LiatrioKeyPairFinal --query 'KeyMaterial' --output text > ~/.ssh/LiatrioKeyPairFinal.pem
 chmod 400 ~/.ssh/LiatrioKeyPairFinal.pem
 python transpose.py terraform/liatrio-ec2.tf
--- liatrio-infra-automation/dumpJSONDescribeInstances.sh
+++ liatrio-infra-automation/dumpJSONDescribeInstances.sh
@@ -1,4 +1,4 @@
 aws ec2 describe-instances > instances.json
-export TARGETIP=`python getIPFromRunningInstance.py instances.json`
+export TARGETIP=`python getIPFromRunningInstance.py instances.json | tail -1`
 sed "s#TGTHOST#$TARGETIP#g" jenkinsHostTemplate > ./ansible-playbooks/inventory/template-env/hosts
 echo $TARGETIP > .instanceip
