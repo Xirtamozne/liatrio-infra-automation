2fce621b6ef63294100b6bb9a6a8b02046d03c31
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 01:52:53 +0200

    Added Java install to postInstall and Jenkins update

--- liatrio-infra-automation/postInstall.sh
+++ liatrio-infra-automation/postInstall.sh
@@ -1,9 +1,11 @@
+add-apt-repository ppa:webupd8team/java
+echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 apt -y update && apt install -y python
 apt -y install python-pip
 pip install requests
 pip install httplib2
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 chown ubuntu /etc/apt/sources.list
-echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 apt-get update
-apt-get install jenkins
+apt install oracle-java8-installer
+apt-get -y install jenkins
