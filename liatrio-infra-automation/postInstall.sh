e8ae8938c3c999b7d2c27ebb5e2dc89e657ff7d5
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 01:40:07 +0200

    Fix Jenkins install... hopefully?

--- liatrio-infra-automation/postInstall.sh
+++ liatrio-infra-automation/postInstall.sh
@@ -2,4 +2,8 @@ apt -y update && apt install -y python
 apt -y install python-pip
 pip install requests
 pip install httplib2
-apt -y install jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+chown ubuntu /etc/apt/sources.list
+echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
+apt-get update
+apt-get install jenkins
