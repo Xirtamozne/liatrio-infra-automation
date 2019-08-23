c9afeaaebf8fb53354b51e566630e929c7440c66
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 03:17:36 +0200

    Changing place to stage nexus tarball, trying to avoid a duplicate nexus deployment

--- liatrio-infra-automation/bootstrap-nexus.sh
+++ liatrio-infra-automation/bootstrap-nexus.sh
@@ -1,2 +1,1 @@
-wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.7.1-02-unix.tar.gz
 ansible-playbook -vvvvi .instanceip.yml ansible-playbooks/install-nexus.yml
--- liatrio-infra-automation/deployInfra.sh
+++ liatrio-infra-automation/deployInfra.sh
@@ -8,9 +8,8 @@ scp -i ~/.ssh/LiatrioKeyPairFinal.pem ./postInstall.sh ubuntu@`cat .instanceip`:
 sleep 5
 ssh -ttqi ~/.ssh/LiatrioKeyPairFinal.pem ubuntu@`cat .instanceip` "sudo /home/ubuntu/postInstall.sh"
 sleep 5
+wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.7.1-02-unix.tar.gz
 ./bootstrap-jenkins.sh
-sleep 5
-./bootstrap-nexus.sh
 sleep 5
 ./bootstrap-nexus.sh
 sleep 5
