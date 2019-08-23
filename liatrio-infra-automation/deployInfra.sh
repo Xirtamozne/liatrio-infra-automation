7b1956a282cac8c90b84d85b7ea6f4a267a0d900
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 02:40:54 +0200

    Adding pauses between steps

--- liatrio-infra-automation/deployInfra.sh
+++ liatrio-infra-automation/deployInfra.sh
@@ -1,11 +1,17 @@
 ./bootstrap-packer.sh
+sleep 5
 ./bootstrap-terraform.sh
 sleep 5
 ./dumpJSONDescribeInstances.sh
+sleep 5
 scp -i ~/.ssh/LiatrioKeyPairFinal.pem ./postInstall.sh ubuntu@`cat .instanceip`:/home/ubuntu/
+sleep 5
 ssh -ttqi ~/.ssh/LiatrioKeyPairFinal.pem ubuntu@`cat .instanceip` "sudo /home/ubuntu/postInstall.sh"
 sleep 5
 ./bootstrap-jenkins.sh
+sleep 5
 ./bootstrap-nexus.sh
+sleep 5
 ./bootstrap-nexus.sh
+sleep 5
 ./postDeploy.py
