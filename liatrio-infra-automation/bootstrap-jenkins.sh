d2706fbfc9ccbb54fab4a299cbd1c4f74536d922
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 02:27:37 +0200

    More tweaks. Timing runs. Optimization.

--- liatrio-infra-automation/bootstrap-jenkins.sh
+++ liatrio-infra-automation/bootstrap-jenkins.sh
@@ -1,1 +1,2 @@
+ansible-playbook -vvvvi .instanceip.yml ansible-playbooks/install-jdk.yml
 ansible-playbook -vvvvi .instanceip.yml ansible-playbooks/install-jenkins.yml
--- liatrio-infra-automation/postInstall.sh
+++ liatrio-infra-automation/postInstall.sh
@@ -1,11 +1,4 @@
-add-apt-repository ppa:webupd8team/java
-echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 apt -y update && apt install -y python
 apt -y install python-pip
 pip install requests
 pip install httplib2
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-chown ubuntu /etc/apt/sources.list
-apt-get update
-apt install oracle-java8-installer
-apt-get -y install jenkins
