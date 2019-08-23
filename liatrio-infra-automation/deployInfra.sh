1dbdc1f69158a7e4b6367a97d3e7f2464ad48982
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-23 03:27:05 +0200

    Stripped some Jenkins plugins to cut down on install time. Added date/time stats to deployer.

--- liatrio-infra-automation/deployInfra.sh
+++ liatrio-infra-automation/deployInfra.sh
@@ -1,6 +1,15 @@
+echo RUN STARTED:   
+date
+echo "================================="
 ./bootstrap-packer.sh
+echo "Packer step complets: " 
+date
+echo "================================="
 sleep 5
 ./bootstrap-terraform.sh
+echo "Terraform step complets: " 
+date
+echo "================================="
 sleep 5
 ./dumpJSONDescribeInstances.sh
 sleep 5
@@ -10,7 +19,18 @@ ssh -ttqi ~/.ssh/LiatrioKeyPairFinal.pem ubuntu@`cat .instanceip` "sudo /home/ub
 sleep 5
 wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.7.1-02-unix.tar.gz
 ./bootstrap-jenkins.sh
+date
+echo "Jenkins step complets: " 
+echo "================================="
 sleep 5
 ./bootstrap-nexus.sh
 sleep 5
+echo "Nexus step complets: " 
+date
+echo "================================="
 ./postDeploy.py
+echo "Run completes... "
+date
+echo "================================="
+echo "Instance up: "
+cat .instanceip
