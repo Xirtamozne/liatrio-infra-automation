a7a07666538b063c74f44f9d4d6275121816815a
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-27 02:25:14 -0600

    Spelling...

--- liatrio-infra-automation/deployInfra.sh
+++ liatrio-infra-automation/deployInfra.sh
@@ -2,12 +2,12 @@ echo RUN STARTED:
 date
 echo "================================="
 ./bootstrap-packer.sh
-echo "Packer step complets: " 
+echo "Packer step complete: " 
 date
 echo "================================="
 sleep 5
 ./bootstrap-terraform.sh
-echo "Terraform step complets: " 
+echo "Terraform step complete: " 
 date
 echo "================================="
 sleep 5
@@ -21,12 +21,12 @@ wget http://mirrors.jenkins-ci.org/plugins/nexus-jenkins-plugin/3.5.20190425-152
 wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.7.1-02-unix.tar.gz
 ./bootstrap-jenkins.sh
 date
-echo "Jenkins step complets: " 
+echo "Jenkins step complete: " 
 echo "================================="
 sleep 5
 ./bootstrap-nexus.sh
 sleep 5
-echo "Nexus step complets: " 
+echo "Nexus step complete: " 
 date
 echo "================================="
 ./postDeploy.py
