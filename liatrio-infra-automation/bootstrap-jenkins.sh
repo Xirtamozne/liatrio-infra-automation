008eb5e4c64fc73ac47b793e04f56fd9de9f01ce
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-27 02:29:01 -0600

    Update bootstrap-jenkins.sh

--- liatrio-infra-automation/bootstrap-jenkins.sh
+++ liatrio-infra-automation/bootstrap-jenkins.sh
@@ -1,2 +1,1 @@
-ansible-playbook -vvvvi .instanceip.yml ansible-playbooks/install-jdk.yml
-ansible-playbook -vvvvi .instanceip.yml ansible-playbooks/install-jenkins.yml
+ansible-playbook -i .instanceip.yml ansible-playbooks/install-jenkins.yml
