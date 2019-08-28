b8d3e217d6e63e11e099398636725efd4b2388dd
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-28 12:23:42 -0600

    Update dumpJSONDescribeInstances.sh

--- liatrio-infra-automation/dumpJSONDescribeInstances.sh
+++ liatrio-infra-automation/dumpJSONDescribeInstances.sh
@@ -3,3 +3,9 @@ export TARGETIP=`python getIPFromRunningInstance.py instances.json | tail -1`
 sed "s#TGTHOST#$TARGETIP#g" jenkinsHostTemplate > ./ansible-playbooks/inventory/template-env/hosts
 sed "s#TGTHOST#$TARGETIP#g" .instanceip.template > .instanceip.yml
 echo $TARGETIP > .instanceip
+sleep 3
+scp -i ~/.ssh/LiatrioKeyPairFinal.pem ./postInstall.sh ubuntu@$TARGETIP:/home/ubuntu/
+sleep 3
+scp -i ~/.ssh/LiatrioKeyPairFinal.pem ./postInstall.sh ubuntu@$TARGETIP:/home/ubuntu/
+sleep 3
+ssh -ttqi ~/.ssh/LiatrioKeyPairFinal.pem ubuntu@$TARGETIP "sudo -H /home/ubuntu/postInstall.sh"
