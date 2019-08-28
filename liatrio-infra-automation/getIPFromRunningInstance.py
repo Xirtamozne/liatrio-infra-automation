f3da35a1aa02ceb6fdcdc541f3286b5fa18abfbf
Xirtamozne <Johnny.E.Hughes@gmail.com>
2019-08-28 10:23:11 -0600

    Update getIPFromRunningInstance.py

--- liatrio-infra-automation/getIPFromRunningInstance.py
+++ liatrio-infra-automation/getIPFromRunningInstance.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
-## Quick and dirty code-snippet to grab the external IP from the actively running instance with the appropriate tag.
+# Gets most recently launched public IP, still potential for data race
 
 import json
 import sys
+import datetime
+import calendar
+format = '%Y-%m-%dT%H:%M:%S.000Z'
+
+def getTimeStamp(datestring):
+    #datestring = "2019-08-27T09:51:34.000Z"
+    d = datetime.datetime.strptime(datestring, format)
+    return calendar.timegm(d.timetuple())
+
+def sortByTime(keyPosition):
+   return keyPosition[1]
 
 try:
    j = json.loads(open(sys.argv[1]).read())
@@ -11,7 +22,11 @@ except:
    print "Read Failure..."
    raise Exception
 
+IPLaunchTimes=[]
 for t in xrange(len(j['Reservations'])):
    if ("running" == j['Reservations'][t]['Instances'][0]['State']['Name']):
       if ("liatrio-jenkins-nexus" == j['Reservations'][t]['Instances'][0]['Tags'][0]['Value']):
-         print j['Reservations'][t]['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicIp']
+         IPLaunchTimes.append( (j['Reservations'][t]['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicIp'] , getTimeStamp(j['Reservations'][t]['Instances'][0]['LaunchTime'])))
+         
+IPLaunchTimes.sort(key=sortByTime)
+print IPLaunchTimes[-1][0]
